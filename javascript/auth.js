7d890b39022796ccb10904d834c7450c5c9ee071
Kris Renaldi <krenaldi@comcast.net>
2019-07-17 16:51:30 -0400

    Added console logs for testing and move firebase init to separate file

--- javascript/auth.js
+++ javascript/auth.js
@@ -1,11 +1,14 @@
 // signup
 const registerForm = document.querySelector('#register-form');
-registerForm.addEventListener('submit', (e) => {
+console.log(registerForm);
+registerForm.addEventListener("submit", (e) => {
   e.preventDefault();
   
   // get user info
-  const email = registerForm['reg-email'].value;
-  const password = registerForm['password'].value;
+  const email = registerForm["reg-email"].value;
+  const password = registerForm["reg-password"].value;
+  console.log(email);
+  console.log(password);
 
   // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
