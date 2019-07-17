55c680ab01cbf5cc6dc4b1c411e7e752613c7ab4
ahp336 <aakashparikh8@gmail>
2019-07-16 20:16:07 -0400

    aakash

---
+++ javascript/app.js
@@ -0,0 +1,33 @@
+//  Your web app’s Firebase configuration
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDHXjpQq1PmJOo5QrvWotIuTwp8rMrNMWk",
+//     authDomain: "project-1-b0475.firebaseapp.com",
+//     databaseURL: "https://project-1-b0475.firebaseio.com",
+//     projectId: "project-1-b0475",
+//     storageBucket: "",
+//     messagingSenderId: "601874914722",
+//     appId: "1:601874914722:web:3fdce380a602b1c7"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   var database = firebase.database();
+
+//   event.preventDefault();
+
+// var firstName = ""
+// var lastName = ""
+// var email = ""
+// var password = ""
+
+// $("#lg-reg").on("click", function(){
+// event.preventDefault();
+
+//     var firstName = $("#firstnameInput").val().trim();
+//     var lastName = $("#lastnameInput").val().trim();
+//     var email = $("#reg-email").val().trim();
+//     var password = $("#password").val().trim();
+
+
+
+// });
+
