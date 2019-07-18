7eb2a9de071c2fe2f13f2c7f4ac2b452557c0f4e
Kris Renaldi <krenaldi@comcast.net>
2019-07-18 01:06:30 -0400

    Added functions to add attribute to Login and Register buttons, trigger click event, and then remove the attribute all so that modal will close after login and creating new user

--- javascript/auth.js
+++ javascript/auth.js
@@ -1,19 +1,34 @@
+const regButton = $('#lg-reg');
+//console.log(regButton);
+const logButton = $("#lg-log");
+
+function closeRegModal(){
+    regButton.attr("data-dismiss", "modal");
+    regButton.trigger('click');
+    regButton.removeAttr("data-dismiss", "modal");
+}
+
+function closeLogModal(){
+    logButton.attr("data-dismiss", "modal");
+    logButton.trigger('click');
+    logButton.removeAttr("data-dismiss", "modal");
+}
+
 // signup
 const registerForm = document.querySelector('#register-form');
-console.log(registerForm);
-registerForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = registerForm["reg-email"].value;
-  const password = registerForm["reg-password"].value;
-  console.log(email);
-  console.log(password);
-
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-    // reset form
-    registerForm.reset();
-  });
+// console.log("Hello");
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // console.log("Hello again");
+    // get user info
+    const email = registerForm['reg-email'].value;
+    const password = registerForm['reg-password'].value;
+    
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        // reset form
+        registerForm.reset();
+    });
+    closeRegModal();
 });
